756adb2ff1c779ff9e54ab2154e9d4b70a4c0927
Jonathan Bugs <jonathan@cssroots.com.br>
2013-08-01 17:44:33 -0300

    ultimas alteracoes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
-	imgRetina();
+	menuMobile();
+	//imgRetina();
+	placeHolder();
 });
 
+
+function menuMobile(){
+	$('.showMenu, .fecharMenu').on('click', function(){
+		$('html, body, #wrapper, #menuUl, #boxLogo .logo, .linkEn, .fecharMenu').toggleClass('menuAberto');
+	});
+}
+
+
+/**************
+	IMAGENS RETINA
+**************/
 function imgRetina(){
 	Retina = function() {
 		return {
@@ -20,3 +33,65 @@ function imgRetina(){
 	}();
 	Retina.init();
 }
+
+
+
+/**************
+	PLACEHOLDER
+**************/
+function placeHolder(){
+	$('.input, .textarea').on('keyup',function(){
+		if($(this).val()===''){ $(this).prev().show(); }
+	}).on('keydown',function(){
+		$(this).prev().hide();
+	}).on('change',function(){
+		if ($(this).val()===''){ $(this).prev().show(); } else { $(this).prev().hide(); }
+	}).on('focusout',function(){
+		$(this).prev().fadeTo(0,1);
+		if ($(this).val()===''){ $(this).prev().show(); $(this).parent().find('.erro').show(); } else { $(this).prev().hide(); $(this).parent().find('.erro').hide(); }
+	}).on('focusin',function(){
+		if ($(this).val()===''){ $(this).prev().show(); $(this).prev().fadeTo(0,0.3); } else { $(this).prev().hide(); }
+	}).each(function(){
+		if ($(this).val()===''){ $(this).prev().show(); } else { $(this).prev().hide(); }
+	});
+}
+
+
+/**************
+	VALIDACAO DE FORMULARIO 
+**************/
+// function validaForm(){
+// 	$('#formContato').validate({
+// 		rules: {
+// 			iptNome:{ required: true },
+// 			iptEmail:{ required: true, email: true },
+// 			textAreaMensagem:{ required: true }
+// 		},
+// 		submitHandler:function() {
+// 			enviaContato();
+// 		},
+// 	});
+// 	$('#btEnviar').on('click', function(){
+// 		setTimeout(function(){
+// 			$('label.error').siblings('label, span').css({'color':'#FF0000'});
+// 		}, 100);
+// 	});
+// }
+
+// function enviaContato(){
+// 	var nome        = $("#iptNome").val();
+// 	var email       = $("#iptEmail").val();
+// 	var mensagem    = $("#textAreaMensagem").val();
+
+// 	$.post("./ajax/_post.contato.php", {
+// 		'nome'        : nome,
+// 		'email'       : email,
+// 		'mensagem'    : mensagem
+// 	},
+// 	function(data){
+// 		$("#nome").val('').prev().show();
+// 		$("#email").val('').prev().show();
+// 		$("#mensagem").val('').prev().show();
+// 	}, "html");
+// 	alert('Sua mensagem foi enviada com sucesso. Obrigado!');
+// }
